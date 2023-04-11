7b2c9160748393c4e2da03cfb0453d272476b7a9
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-04-11 12:09:47 -0600

    creating function for each type

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hola!');
+function numberIdentity(item: number):number {
+  return item;
+}
+
+function stringIdentity(item: string):string {
+  return item;
+}
+
+
+function booleanIdentity(item: boolean):boolean {
+  return item;
+}
